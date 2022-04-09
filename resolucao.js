da52652369c59d05dd7c12b7e9ff658b593903d1
Higor Gonçalves Brandão <higorb2000@gmail.com>
2022-04-09 18:32:50 -0300

    feat: função de exibição do estoque total por categoria

--- resolucao.js
+++ resolucao.js
@@ -1,10 +1,10 @@
 
 const fs = require('fs');
 let brokenDatabase = require('./broken-database.json');
-const characterFixer = { 'æ' : 'a', '¢' : 'c', 'ø' : 'o', 'ß' : 'b' };
+const characterFixer = { 'æ': 'a', '¢': 'c', 'ø': 'o', 'ß': 'b' };
 
-let fixedDatabase = brokenDatabase.map((product) => {
-    
+let fixedDatabase = brokenDatabase.map(product => {
+
     fixProductName(product);
     fixPriceType(product);
     fixProductQuantity(product);
@@ -18,14 +18,14 @@ writeFixedDatabaseFile(fixedDatabase);
 function fixProductName(product) {
 
     product.name = product.name.replace(/[æ|¢|ø|ß]/g, charactersToReplace => (characterFixer)[charactersToReplace]);
-    
+
 }
 
 function fixPriceType(product) {
 
     if(typeof(product.price) === 'string') {
 
-        product.price = parseInt(product.price);
+        product.price = parseFloat(product.price);
 
     }
 
@@ -55,18 +55,21 @@ function writeFixedDatabaseFile(database) {
 
 }
 
+printProductsInOrder(fixedDatabase);
+totalStockValueByCategory(fixedDatabase);
+
 function printProductsInOrder(database) {
 
     database.sort((a, b) => {
-        
-        return((a.category > b.category) ? 1 : (a.category < b.category) ? -1 : 0)
+
+        return ((a.category > b.category) ? 1 : (a.category < b.category) ? -1 : 0)
 
     }).sort((a, b) => {
 
         if(a.category === b.category) {
 
-            return((a.id > b.id) ? 1 : (a.id < b.id) ? -1 : 0)
-            
+            return ((a.id > b.id) ? 1 : (a.id < b.id) ? -1 : 0)
+
         }
 
     })
@@ -77,8 +80,27 @@ function printProductsInOrder(database) {
 
 function totalStockValueByCategory(database) {
 
+    let totalCategory = {}
+
+    database.map(product => {
+
+        let category = product.category
+
+        if(!totalCategory[category]) {
+
+            totalCategory[product.category] = product.quantity
+
+        } else {
+
+            let total = totalCategory[product.category]
+            totalCategory[product.category] = total + product.quantity
+
+        }
+
+    })
+
+    console.log(totalCategory)
 
 }
 
-printProductsInOrder(fixedDatabase)
-totalStockValueByCategory(fixedDatabase)
+
